6479b87767ecbf7bed9784e5276b789adaa55d11
acmors <marcosantoniosilva047@gmail.com>
2025-05-04 18:44:37 -0300

    entities

--- SimuladorExtrato/src/model/ContaFGTS.java
+++ SimuladorExtrato/src/model/ContaFGTS.java
@@ -1,4 +1,51 @@
 package model;
 
 public class ContaFGTS {
+
+    private int id;
+    private String numeroConta;
+    private double saldo;
+    private Usuario usuario;
+
+    public ContaFGTS() {
+    }
+
+    public ContaFGTS(int id, double saldo, String numeroConta, Usuario usuario) {
+        this.id = id;
+        this.saldo = saldo;
+        this.numeroConta = numeroConta;
+        this.usuario = usuario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
